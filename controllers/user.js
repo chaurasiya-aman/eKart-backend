8197f111b9997ed3b47ec367c727643f6e405147
Aman <amanchaurasiya2207@gmail.com>
2026-04-02 21:02:29 +0530

    resolve the cookie problem

--- controllers/user.js
+++ controllers/user.js
@@ -220,12 +220,14 @@ export const login = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true in production (HTTPS)
+      sameSite: "None",
       maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
       maxAge: 20 * 24 * 60 * 60 * 1000, // 20 days
     });
 
